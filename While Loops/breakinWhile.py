903b41021c6d861bf8538d2287ac95dfe3a3834a
Birendra Bahadur Budha <birendrabahadurbudhaeng99@gmail.com>
2024-02-18 19:40:58 +0530

    Adding While Loops File

---
+++ While Loops/breakinWhile.py
@@ -0,0 +1,14 @@
+x=10
+
+# while x<=20:
+#     if(x==15):
+#         continue
+#     print(x)
+#     x+=1
+
+a=5
+while(a<=10):
+    a+=1
+    if(a==7):
+        continue
+    print(a)
---
+++ While Loops/while_Else.py
@@ -0,0 +1,7 @@
+
+x=10
+while x<=20:
+    print(x)
+    x+=1
+else:
+    print("Finshed")
